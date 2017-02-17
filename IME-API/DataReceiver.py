2fffe017c68e65c90ed450764f74ea1eb82d7a2a
leomari <mari.leonhardsen@gmail.com>
2017-02-17 12:38:22 +0100

    til live

--- IME-API/DataReceiver.py
+++ IME-API/DataReceiver.py
@@ -5,27 +5,30 @@ class DataReceiver ():
 
 
     # TODO: Implement this
-    def getExamDate(self, codeInput) -> str:
-
-
+    def getExamDate(self, code) -> str:
         base_url = "http://www.ime.ntnu.no/api/course/en/"
-
-        # Fetch the course
-        course = requests.get(base_url + codeInput).json()
-        course2 = json.dumps(course, indent=4)
-        print(course2)
-        print(course2)
         #code = input("Please provide a course code: ")
 
+        # Fetch the course
+        course = requests.get(base_url + code).json()
+
+        view = json.dumps(course, indent=4)
+        print(view)
+        sjekk=course
+        print(sjekk)
+
         # Get relevant data and print it
+        code = course["course"]["code"]
+        name = course["course"]["name"]
         exam_date = course["course"]["assessment"][0]["date"]
 
-        print(exam_date + " test")
+        print("Exam date for", code, name, "is", exam_date)
 
-        return ("Exam date for", code, name, "is", exam_date)
+
+
 
 data = DataReceiver()
-data.getExamDate('TDT4140')
+data.getExamDate('TDT4100')
 
 
 
