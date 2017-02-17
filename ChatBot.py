0d97e45e880aadcc7331f6e9b6ea28a93f0cbb17
leomari <mari.leonhardsen@gmail.com>
2017-02-17 12:22:03 +0100

    Rotet i koden fra its

--- ChatBot.py
+++ ChatBot.py
@@ -5,3 +5,5 @@ class ChatBot():
         pass
 
 
+
+
--- IME-API/DataReceiver.py
+++ IME-API/DataReceiver.py
@@ -1,9 +1,33 @@
+import requests
+import json
 class DataReceiver ():
 
 
+
     # TODO: Implement this
-    def getExamDate(self) -> str:
-        pass
+    def getExamDate(self, codeInput) -> str:
+
+
+        base_url = "http://www.ime.ntnu.no/api/course/en/"
+
+        # Fetch the course
+        course = requests.get(base_url + codeInput).json()
+        course2 = json.dumps(course, indent=4)
+        print(course2)
+        print(course2)
+        #code = input("Please provide a course code: ")
+
+        # Get relevant data and print it
+        exam_date = course["course"]["assessment"][0]["date"]
+
+        print(exam_date + " test")
+
+        return ("Exam date for", code, name, "is", exam_date)
+
+data = DataReceiver()
+data.getExamDate('TDT4140')
+
+
 
 
 
