18458f1a68622be339bba01ffdf4bc216b90c1a2
leomari <mari.leonhardsen@gmail.com>
2017-02-17 13:11:22 +0100

    Ikke-eksamen
    Tar høyde for fag som ikke har eksamen

--- IME-API/DataReceiver.py
+++ IME-API/DataReceiver.py
@@ -7,28 +7,25 @@ class DataReceiver ():
     # TODO: Implement this
     def getExamDate(self, code) -> str:
         base_url = "http://www.ime.ntnu.no/api/course/en/"
-        #code = input("Please provide a course code: ")
 
         # Fetch the course
-        course = requests.get(base_url + code).json()
+        subject = requests.get(base_url + code).json()
 
-        view = json.dumps(course, indent=4)
-        print(view)
-        sjekk=course
-        print(sjekk)
+
+
 
         # Get relevant data and print it
-        code = course["course"]["code"]
-        name = course["course"]["name"]
-        exam_date = course["course"]["assessment"][0]["date"]
+        code = subject["course"]["code"]
+        name = subject["course"]["name"]
 
-        print("Exam date for", code, name, "is", exam_date)
+        try:
+                exam_date = subject["course"]["assessment"][0]["date"]
+                return ("Exam date for", code, name, "is", exam_date)
 
 
+        except KeyError:
+                return("No exam date available")
 
-
-data = DataReceiver()
-data.getExamDate('TDT4100')
 
 
 
