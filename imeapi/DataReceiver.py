a6144e433217e31abb3ecd8ed9d714a717e8319c
leomari <mari.leonhardsen@gmail.com>
2017-02-22 20:11:24 +0100

    Apply PEP8

--- imeapi/DataReceiver.py
+++ imeapi/DataReceiver.py
@@ -2,9 +2,9 @@ import requests
 import json
 
 base_url = "http://www.ime.ntnu.no/api/course/en/"
+class DataReceiver ():
 
 
-class DataReceiver:
     @staticmethod
     def get_data(course_code: str):
         data = requests.get(base_url + course_code).json()
@@ -13,164 +13,116 @@ class DataReceiver:
     @staticmethod
     def get_exam_date(course_code: str) -> str:
 
-        subject = DataReceiver.get_data(course_code)
+        data = DataReceiver.get_data(course_code)
 
-        # Get relevant data and print it
-        course_code = subject["course"]["code"]
-        name = subject["course"]["name"]
+        # Get relevant data
+        name = data["course"]["name"]
         try:
-            exam_date = subject["course"]["assessment"][0]["date"]
+            exam_date = data["course"]["assessment"][0]["date"]
             return "Exam date for " + str(course_code) + " " + str(name) + " is " + str(exam_date)
         except KeyError:
-            return "No Exam date available"
+            #todo: add reason, e.g. wrong semester, different evaluation form
+            return "No exam date available"
 
-    def getContactInfo(self, codeInput) -> str:
+    @staticmethod
+    def get_contact_name(course_code)-> str:
 
         # Fetch the course
-        data = self.getData(codeInput)
-        data2 = json.dumps(data, indent=4)
-        # print(data2)
+        data = DataReceiver.get_data(course_code)
 
-        # Get relevant data and print it
-        personData = data["course"]["educationalRole"][0]["person"]
+        return data["course"]["educationalRole"][0]["person"]["displayName"]
 
-        name = personData["displayName"]
-        print()
-        print()
-        print(name)
-        mail = personData["email"]
-        print(mail)
-        # office = personData["officeAddress"]
-        # print (office)
-        phone = personData["phone"]
-        print(phone)
+    @staticmethod
+    def get_contact_mail(course_code)-> str:
+
+        # Fetch the course
+        data = DataReceiver.get_data(course_code)
+
+        return data["course"]["educationalRole"][0]["person"]["email"]
+
+    @staticmethod
+    def get_contact_office(course_code)-> str:
+
+        # Fetch the course
+        data = DataReceiver.get_data(course_code)
+
+        return data["course"]["educationalRole"][0]["person"]["officeAddress"]
+
+    @staticmethod
+    def get_contact_phone(course_code)-> str:
+
+        # Fetch the course
+        data = DataReceiver.get_data(course_code)
+
+        return data["course"]["educationalRole"][0]["person"]["phone"]
+
+    @staticmethod
+    def get_contact_website(course_code)-> str:
+        #gets the contacts person website
+
+        mail = DataReceiver.get_contact_mail(course_code)
 
         website_id = mail.split("@")
 
-        base_url_ntnu = "https://www.ntnu.no/ansatte/"
-        website = base_url_ntnu + website_id[0]
+        return "https://www.ntnu.no/ansatte/" + website_id[0]
 
-        print(website)
-
-        return ("Contact info for TMA4105 is", name)
-
-    def getCourseName(self, codeInput) -> str:
+    @staticmethod
+    def get_course_name(course_code)-> str:
 
         # Fetch the course
-        data = self.getData(codeInput)
-        data2 = json.dumps(data, indent=4)
-        # print(data2)
+        data = DataReceiver.get_data(course_code)
 
-        # Get relevant data and print it
-        courseName = data["course"]["norwegianName"]
+        return data["course"]["englishName"]
 
-        print(courseName)
-
-        return courseName
-
-    def getCredit(self, codeInput) -> str:
+    @staticmethod
+    def get_credit(course_code)-> str:
 
         # Fetch the course
-        data = self.getData(codeInput)
-        data2 = json.dumps(data, indent=4)
-        # print(data2)
+        data = DataReceiver.get_data(course_code)
 
-        # Get relevant data and print it
-        credit = data["course"]["credit"]
+        return data["course"]["credit"]
 
-        print(credit)
 
-        return credit
-
-    def getURL(self, codeInput) -> str:
+    @staticmethod
+    def get_URL(course_code)-> str:
 
         # Fetch the course
-        data = self.getData(codeInput)
-        data2 = json.dumps(data, indent=4)
-        # print(data2)
+        data = DataReceiver.get_data(course_code)
 
-        # Get relevant data and print it
-        url = data["course"]["infoType"][0]["text"]
+        return data["course"]["infoType"][0]["text"]
 
-        print(url)
 
-        return url
-
-    def getFORK(self, codeInput) -> str:
+    @staticmethod
+    def get_prerequisite_knowledge(course_code)-> str:
 
         # Fetch the course
-        data = self.getData(codeInput)
-        data2 = json.dumps(data, indent=4)
-        # print(data2)
+        data = DataReceiver.get_data(course_code)
 
-        # Get relevant data and print it
-        # have to fix
-        fork = data["course"]["infoType"][1]["code"]
+        # Get relevant data
+        return data["course"]["infoType"][1]["code"]
 
-        print(fork)
-
-        return fork
-
-    def getContent(self, codeInput) -> str:
-
-        # Returns the course info in English by default.
-        # Implement a Norwegian version
-
+    @staticmethod
+    def get_course_content(course_code)-> str:
 
         # Fetch the course
-        data = self.getData(codeInput)
-        data2 = json.dumps(data, indent=4)
+        data = DataReceiver.get_data(course_code)
 
-        # Get relevant data and print it
-        content = data["course"]["infoType"][2]["text"]
+        return data["course"]["infoType"][2]["text"]
 
-        print(content)
 
-        return content
-
-    def getCourseMaterial(self, codeInput) -> str:
-        # Returns the course info in English by default.
-        # Implement a Norwegian version
-
+    @staticmethod
+    def get_course_material(course_code) -> str:
 
         # Fetch the course
-        data = self.getData(codeInput)
-        data2 = json.dumps(data, indent=4)
+        data = DataReceiver.get_data(course_code)
 
-        # Get relevant data and print it
-        material = data["course"]["infoType"][3]["text"]
+        return data["course"]["infoType"][3]["text"]
 
-        print(material)
 
-        return material
-
-    def getForm(self, codeInput) -> str:
-        # Returns the course info in English by default.
-        # Implement a Norwegian version
-
+    @staticmethod
+    def get_teaching_form(course_code) -> str:
 
         # Fetch the course
-        data = self.getData(codeInput)
-        data2 = json.dumps(data, indent=4)
+        data = DataReceiver.get_data(course_code)
 
-        # Get relevant data and print it
-        form = data["course"]["infoType"][4]["text"]
-
-        print(form)
-
-        return form
-
-    def getANBFORK(self, codeInput) -> str:
-        # Returns the course info in English by default.
-        # Implement a Norwegian version
-
-        # Fetch the course
-        data = self.getData(codeInput)
-        data2 = json.dumps(data, indent=4)
-
-        # Get relevant data and print it
-        anbfork = data["course"]["infoType"][6]["text"]
-
-        print(anbfork)
-
-        return anbfork
+        return data["course"]["infoType"][4]["text"]
