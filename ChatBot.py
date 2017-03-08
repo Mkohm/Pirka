7d51cc5ecd966027d834d781ff56d258de2abb75
Mkohm <marius.kohmann@gmail.com>
2017-03-08 21:34:20 +0100

    different users can login

--- ChatBot.py
+++ ChatBot.py
@@ -2,7 +2,6 @@ import json
 import os
 from flask import Flask
 from flask import request
-import requests
 from flask import make_response
 from flask import render_template
 from scraper.BlackboardScraper import BlackboardScraper
@@ -13,13 +12,9 @@ from imeapi.Course import Course
 app = Flask(__name__)
 app.debug = True
 
-
-
-facebook_id = {}
-current_sender_id = None
-
 # Change this variable to true if you are going to run this on Heroku
 deployment = False
+pirka_users = {}
 
 if deployment:
     deployment_link = "webhook"
@@ -33,6 +28,7 @@ class ChatBot:
         # Change this variable to true if you are going to run this on Heroku
         self.deployment = False
 
+
         # The port to run webserver on
         self.port = int(os.getenv('PORT', 8080))
 
@@ -43,22 +39,42 @@ class ChatBot:
     # Receives action-name, gets the data and returns a string ready to send back to API.AI
     @staticmethod
     def process_actions(parameter: str, action_name: str) -> str:
-
-        if action_name == "get_exam_date":
-            return Course(parameter).get_exam_date()
-        if action_name == "get_assessment_form":
-            return Course(parameter).get_assessment_form()
-        elif action_name == "login":
-            #save user data
-            pass
+        if action_name == "login":
+            return ChatBot.create_followup_event_data(parameter)
+        elif action_name == "get_exam_date":
+            return ChatBot.create_data(Course(parameter).get_exam_date())
+        elif action_name == "get_assessment_form":
+            return ChatBot.create_data(Course(parameter).get_assessment_form())
         else:
-            return "I didnt understand shit, you probably broke me :("
+            return "I didn't understand shit, you probably broke me :("
+
+    @staticmethod
+    def create_data(speech: str) -> str:
+        data = {
+            "speech": speech,
+            "displayText": speech,
+            # "data": data,
+            # "contextOut": [],
+            "source": "Pirka-chatbot-webserver"
+        }
+        return data
+
+    @staticmethod
+    def create_followup_event_data(parameter_value: str):
+        data = {
+            "followupEvent": {
+                "name": "custom_event",
+                "data": {
+                    "user_id": parameter_value
+                }
+            }
+        }
+        return data
 
 
+@app.route('/login/<current_sender_id>', methods=['POST', 'GET'])
+def login(current_sender_id):
 
-
-@app.route('/login', methods=['POST', 'GET'])
-def login():
     """
     This method handles the login so we can get user information to the blackboard scraper.
     We load a template so the user can login and send us the email and password.
@@ -68,9 +84,13 @@ def login():
     error = None
     if request.method == 'POST':
 
-        #If the login is successfull we return a template saying you can start using pirka
-        if valid_login(request.form['username'], request.form['password']):
+        username = request.form["username"]
+        password = request.form["password"]
 
+        #If the login is successfull we return a template saying you can start using pirka
+        if valid_login(username, password):
+
+            pirka_users[current_sender_id] = {username, password}
             return render_template("login_success.html")
         else:
             error = 'Invalid username/password'
@@ -83,11 +103,6 @@ def valid_login(username:str, password:str):
 
     try:
         scraper = BlackboardScraper(username, password)
-        facebook_id[current_sender_id] = {username, password}
-
-
-        print()
-        print(facebook_id)
         return True
     except:
         return False
@@ -100,91 +115,23 @@ def webhook():
     # Extract the data from the json-request (first get the result section of the json)
     result = json_request.get("result")
 
-
-
-    # Then get the parameters of the result
+    # Then get the parameters and action_name of the result
     parameters = result.get("parameters")
-    parameter = parameters.get("course_code")
 
     action_name = result.get("action")
 
+    #Handles different parameters to the process-actions method
     if action_name == "login":
-        print(json_request)
-        facebook_sender_id = result.get("contexts")[0].get("parameters").get("facebook_sender_id")
-        current_sender_id = facebook_sender_id
-
-
-    data = None
-    if action_name == "loginlogin":
-        print("loginlogin")
-        facebook_sender_id = result.get("contexts")[0].get("parameters").get("facebook_sender_id")
-        print(facebook_sender_id)
-
-        data = {
-                                                "id": "0cdb5479-fb34-489f-8631-d733308a1b3f",
-                                      "timestamp": "2017-03-08T18:15:54.828Z",
-                                      "lang": "en",
-                                      "result": {
-                                        "source": "agent",
-                                        "resolvedQuery": "login",
-                                        "action": "login",
-                                        "actionIncomplete": "false",
-                                        "parameters": {},
-                                        "contexts": [],
-                                        "metadata": {
-                                          "intentId": "580b1de5-9a96-439b-a450-f7ce6ad9b2e5",
-                                          "webhookUsed": "true",
-                                          "webhookForSlotFillingUsed": "false",
-                                          "intentName": "login"
-                                        },
-                                        "fulfillment": {
-                                          "speech": "",
-                                          "messages": [
-                                            {
-                                              "type": "0",
-                                              "speech": ""
-                                            },
-                                            {
-                                              "title": "Itslearning integration",
-                                              "subtitle": "login",
-                                              "imageUrl": "https://raw.githubusercontent.com/Mkohm/Pirka/dev/login.png",
-                                              "buttons": [
-                                                {
-                                                  "text": "Click here to enable itslearning integration",
-                                                  "postback": "http://localhost:8080/login/detteersmud"
-                                                }
-                                              ],
-                                              "type": "1"
-                                            }
-                                          ]
-                                        },
-                                        "score": "1"
-                                      },
-                                      "status": {
-                                        "code": "206",
-                                        "errorType": "partial_content",
-                                        "errorDetails": "Webhook call failed. Error message: org.springframework.web.client.HttpServerErrorException: 500 INTERNAL SERVER ERROR ErrorId: 17aeb4ac-5cb2-4139-8351-a7854935c094"
-                                      },
-                                      "sessionId": "2edb215c-493a-49d8-b317-6375ef759897"
-        }
-
+        parameter = result.get("contexts")[0].get("parameters").get("facebook_sender_id")
+    elif action_name == "get_exam_date":
+        parameter = parameters.get("course_code")
 
 
     speech = ChatBot.process_actions(parameter, action_name)
 
-    data = {
-        "speech": speech,
-        "displayText": speech,
-        # "data": data,
-        # "contextOut": [],
-        "source": "Pirka-chatbot-webserver"
-    }
 
-
-
-
-    print(json.dumps(data, indent=4))
-    response = json.dumps(data, indent=4)
+    print(json.dumps(speech, indent=4))
+    response = json.dumps(speech, indent=4)
     created_response = make_response(response)
     created_response.headers['Content-Type'] = 'application/json'
 
--- scraper/BlackboardScraper.py
+++ scraper/BlackboardScraper.py
@@ -18,24 +18,6 @@ class BlackboardScraper:
         password.send_keys(self.password)
         password.submit()
 
-        # Go to course list
-        wait = ui.WebDriverWait(driver, 10)
-        wait.until(lambda driver: driver.find_element_by_id('l-header'))  # Wait for the site to load properly
-        driver.get("https://ntnu.itslearning.com/main.aspx?TextURL=Course%2fAllCourses.aspx")  # Go to course list
-        driver.switch_to.frame(
-            driver.find_element_by_name("mainmenu"))  # itslearning uses iframes, let's go to the corrct one
-
-        # Fetch and print courses
-        courses = driver.find_elements_by_css_selector(
-            "td > .ccl-iconlink")  # "ccl-iconlink" is the classname associated with hyperlinks. For courses, these are children of the "td" tag
-        for course in courses:
-            print(course.text)
-
-
-    def is_valid_login_details(self):
-        #Try to login
-        pass
-
 
 
 
