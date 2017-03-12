a6dea70f9febc363b3cdd68f6fb65b7af1168381
Mkohm <marius.kohmann@gmail.com>
2017-03-12 17:22:36 +0100

    change filestructure

--- ChatBot.py
+++ ChatBot.py
@@ -30,7 +30,7 @@ class ChatBot:
 
 
         # Bind to PORT if defined, otherwise default to 5000.
-        port = int(os.environ.get('PORT', 5000))
+        port = int(os.environ.get('PORT', 8080))
         app.run(host='0.0.0.0', port=port)
 
 
