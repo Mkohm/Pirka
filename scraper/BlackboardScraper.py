3484c22c6859c37b64a957539cfbcd4d5be3beb6
Even Kallevik <Even Kallevik>
2017-04-24 16:39:05 +0200

    Major bug fix for BlackboardScraper.py

--- scraper/BlackboardScraper.py
+++ scraper/BlackboardScraper.py
@@ -1,12 +1,12 @@
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
 import os
-from database import DatabaseInserter
 import platform
 
 # DOCUMENTATION: http://selenium-python.readthedocs.io/locating-elements.html
 # When running Selenium it is necessary to close the driver. A call to self.close_driver is needed when done.
 
+# TODO: move these varibles and make them member varibales in the class below if needed @Kohm?
 driver_directory = os.path.dirname(__file__)
 if platform.system() == "Windows":
     relative_path = "chromedriver.exe"
@@ -48,84 +48,83 @@ class BlackboardScraper:
 
         # navigates to the calendar page in BB.
         driver.get("https://ntnu.blackboard.com/webapps/bb-social-learning-BBLEARN/execute/mybb?cmd=display&toolId=calendar-mybb_____calendar-tool")
-        driver.implicitly_wait(5)
-        driver.find_element_by_id("ical")
+        driver.implicitly_wait(2)
+        ical_url = driver.find_element_by_id("ical")
 
-        # TODO: implement the rest of this method
-
-        # TODO: This URL structure can be used to subscribe to a calendar by URL, implement feature
+        # This URL structure can be used to subscribe to a calendar by URL, implement feature
         # https://www.google.com/calendar/render?cid=http://www.example.com/calendar.ics
+
+        return ical_url
 
     def get_course_list(self):
 
+        # makes sure the page is completed loaded before trying to locate elements.
         driver.implicitly_wait(1)
+        # utilize the fact that every link text for active courses contains the current term
         courses = driver.find_elements_by_partial_link_text(self.current_term)
         course_list = []
 
         for course in courses:
             course_list.append(course.text.split()[0])
-            DatabaseInserter.add_user_has_course(self.username, str(course.text.split()[0]))
-
 
         return course_list
-
-
-    def get_course(self, index):
-        driver.get("https://ntnu.blackboard.com/webapps/portal/execute/tabs/tabAction?tab_tab_group_id=_70_1")
-        courses = driver.find_elements_by_partial_link_text(self.current_term)
-
-        courses[index].click()
-
-        driver.find_element_by_id("menuPuller").click()
-        assignment_folder = driver.find_element_by_partial_link_text("Øvinger")
-        assignment_folder.click()
 
     def get_assignments(self, index):
         driver.get("https://ntnu.blackboard.com/webapps/portal/execute/tabs/tabAction?tab_tab_group_id=_70_1")
         courses = driver.find_elements_by_partial_link_text(self.current_term)
 
-        course_code = courses[index].text.split()[0]
+        current_course = courses[index].text.split()[0]
+
+        print("Course: " + current_course)
         courses[index].click()
 
-        try:
-            assignment_folder = driver.find_element_by_partial_link_text("Øvinger")
-        except:
-            driver.find_element_by_id("menuPuller").click()
-            assignment_folder = driver.find_element_by_partial_link_text("Øvinger")
+        driver.find_element_by_id("menuPuller").click()
 
+        assignment_folder = driver.find_element_by_partial_link_text("Øvinger")
         assignment_folder.click()
+
         assignments = driver.find_elements_by_partial_link_text("Øving")
 
-        print("Trying to loop through the assignments")
+        assignment_log = []
+
         for i in range(0, len(assignments)):
 
-            title = assignments[i].text
-            obligatory = True
-            published = None
             try:
-                print("Title: " + title + " (" + course_code + ")")
-                assignments[i].click()
+                assignments = driver.find_element_by_partial_link_text("Øving " + str(i+1))
+            except:
+                pass
+            title = assignments.text
+
+            try:
+                print("Title: " + title + " (" + current_course + ")")
+                assignments.click()
+            except:
+                print("hsad")
+            try:
                 score = driver.find_element_by_id("aggregateGrade")
                 print(score.get_attribute("value"))
+            except:
+                score = None
+            try:
                 max_score = driver.find_element_by_id("aggregateGrade_pointsPossible")
                 print(max_score.text)
+            except:
+                print("Score not available")
 
+            try:
+                info = driver.find_element_by_class_name("assignmentInfo").text
+                print("\n\nInfo: " + info + "\n\n")
 
                 try:
                     details = driver.find_element_by_class_name("detailsHeader")
                     details.click()
-
-                    info = driver.find_element_by_class_name("assignmentInfo")
+                    info = driver.find_element_by_class_name("assignmentInfo").text
                     print("\n\nInfo: " + info + "\n\n")
-                    driver.back()
                 except:
-                    deadline = None
+                    print("fu")
             except:
-                print("Score not available")
-
-            DatabaseInserter.add_assignment_data(course_code, title, i + 1, str(obligatory), published, deadline,
-                                                     "its", "exercise", " ingen ")
-            DatabaseInserter.add_user_completed_assignment(self.username, course_code, i + 1, "exercise", score)
+                print("No deadline found")
+                deadline = None
 
             driver.back()
             assignments = driver.find_elements_by_partial_link_text("Øving")
@@ -136,98 +135,23 @@ class BlackboardScraper:
         for i in range(0, len(self.course_list)):
             self.get_assignments(i)
 
-        # # item = driver.find_elements_by_css_selector("div.cell.gradable")
-        # # driver.implicitly_wait(2)
-        # # timestamp = driver.find_elements_by_class_name("lastActivityDate")
-        # #
-        # #
-        # # for i in range(1, len(item)):
-        # #     print("Item " + str(i) + "\n" + item[i].text)
-        # #     print("Timestamp: " + timestamp[i].text + "\n\n")
-        #
-        # print("Course: " + current_course)
-        # courses[index].click()
-        #
-        # # driver.find_element_by_id("menuPuller").click()
-        #
-        # assignment_folder = driver.find_element_by_partial_link_text("Øvinger")
-        # assignment_folder.click()
-        #
-        # assignments = driver.find_elements_by_partial_link_text("Øving")
-        # print("Num of assign: " + str(len(assignments)))
-        #
-        #
-        # for i in range(1, len(assignments)):
-        #
-        #     print("\nLooking for link 'Øving " + str(i) + "'")
-        #
-        #     try:
-        #         assignment = driver.find_element_by_partial_link_text("Øving " + str(i))
-        #         title = assignment.text
-        #         print("Title: " + title + " (" + current_course + ", i=" + str(i) + ")")
-        #         assignment.click()
-        #         try:
-        #             score = driver.find_element_by_id("aggregateGrade")
-        #             print(score.get_attribute("value"))
-        #         except:
-        #             print("Score not available")
-        #         max_score = driver.find_element_by_id("aggregateGrade_pointsPossible")
-        #         print(max_score.text)
-        #         driver.back()
-        #     except:
-        #         print("Exercise info not available")
-        #         driver.back()
-        #
-        # assignments = driver.find_elements_by_css_selector("sortable.item_row.graded_item_row.row.expanded")
-        #
-        # assignment = assignments[0]
-        # grade = assignment.get_attribute("grade")
-        # print("Grade: " + grade)
-        #
-        #
-        #
-        # # activity = driver.find_elements_by_class_name("activityType")
-        # # category = driver.find_elements_by_class_name("itemCat")
-        # # grades = driver.find_elements_by_class_name("grade")
-        # # grade_score = driver.find_elements_by_css_selector("span.grade")
-        # #
-        # #
-        # # for i in range(0, len(grade_score)):
-        # #     print("Index: " + str(i))
-        # #     print("\nAssignment: " +assignments[i+1].text)
-        # #     print("Activity: " +activity[i].text)
-        # #     try:
-        # #         print("Category: " + category[i].text)
-        # #         print("Grade: " + grades[i].text)
-        # #         print("Grade score: " + grade_score[i].text + "\n")
-        # #     except IndexError:
-        # #         print(" ")
-
-
     def close_driver(self):
         driver.quit()
 
-
 """
-
 user = "evenkal"
 password = input("Password: ")
 myScrape = BlackboardScraper(user, password)
 
 
-
 try:
-    myScrape.get_all_assignments()
+    myScrape.get_assignments(0)
 
     # myScrape.get_completed_assignments()
 except:
     print("failed")
 
-
 myScrape.close_driver()
 
 # myScrape.get_calendar_feed()
-
-
 """
-
